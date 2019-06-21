4c8d4ed62486178a5eacbcddeb4cd5c7ec9e2d1f
Denislav Denov <42144642+denislavdenov@users.noreply.github.com>
2019-06-21 14:19:58 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,8 +9,8 @@ provider "cloudflare" {
 
 resource "cloudflare_record" "foobar" {
   domain = "${var.cloudflare_zone}"
-  name   = "ddenov.tk"
-  value  = "3.218.36.59:3000"
+  name   = "ddenov.tk/notes"
+  value  = "3.218.36.59"
   type   = "A"
   ttl    = 3600
 }
